79de058f317f34ffb22935b80f6b895625b29666
yrojx <yossansr06@gmail.com>
2021-10-27 10:29:14 +0700

    Initial Commit

---
+++ Satuju/SatujuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatujuApp.swift
+//  Satuju
+//
+//  Created by Yossan Rahmadi on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SatujuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
