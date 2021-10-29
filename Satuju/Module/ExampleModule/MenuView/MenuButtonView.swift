d7a8542a22daa3ce7059b4b24757c3d2f031f109
PAW123 <gedeprama123@gmail.com>
2021-10-29 14:43:43 +0700

    Menu View Done

---
+++ Satuju/Module/ExampleModule/MenuView/MenuButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  MenuButtonView.swift
+//  Satuju
+//
+//  Created by Gede Wicaksana on 29/10/21.
+//
+
+import SwiftUI
+
+struct MenuButtonView: View {
+    var body: some View {
+        VStack {
+            Button(action: {}, label: {
+                Label(
+                    title: {},
+                    icon: { Image("dotIcon")
+                            .resizable()
+                            .frame(width: 20, height: 5, alignment: .center)
+                            .foregroundColor(.white)
+                            .frame(width: 48, height: 48, alignment: .center)
+                            .background(Color("PurplePrimary"))
+                            .clipShape(Circle())
+                            .padding(9)
+                    }
+                )
+            })
+        }
+    }
+}
+
+struct MenuButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuButtonView()
+    }
+}
---
+++ Satuju/Module/ExampleModule/MenuView/MenuListView.swift
@@ -0,0 +1,81 @@
+//
+//  MenuListView.swift
+//  Satuju
+//
+//  Created by Gede Wicaksana on 29/10/21.
+//
+
+import SwiftUI
+
+struct MenuListView: View {
+    @State var autoPlayCheck: Bool = false
+    @State var detectLanguageCheck: Bool = false
+    init() {
+        UITableView.appearance().backgroundColor = .none
+    }
+    var body: some View {
+        List {
+            Button(action: {autoPlay()}, label: {
+                HStack {
+                    if autoPlayCheck {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.black)
+                    } else {
+                        Image(systemName: "checkmark.circle.fill")
+                            .hidden()
+                    }
+                    Text("Auto Play Translation")
+                        .foregroundColor(.black)
+                        .font(.system(size: 17))
+                }
+            })
+            Button(action: {detectLanguage()}, label: {
+                HStack {
+                    if detectLanguageCheck {
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.black)
+                    } else {
+                        Image(systemName: "checkmark.circle.fill")
+                            .hidden()
+                    }
+                    Text("Detect Language")
+                        .foregroundColor(.black)
+                        .font(.system(size: 17))
+                }
+            })
+            Button(action: {}, label: {
+                HStack {
+                    Image(systemName: "trash")
+                        .foregroundColor(.red)
+                    Text("Clear History")
+                        .foregroundColor(.red)
+                        .font(.system(size: 17))
+                }
+            })
+        }
+        .hasScrollEnabled(false)
+        .cornerRadius(14)
+        .padding()
+        .frame(width: 300, height: 200, alignment: .topLeading)
+    }
+    func autoPlay() {
+        if autoPlayCheck {
+            autoPlayCheck = false
+        } else {
+            autoPlayCheck = true
+        }
+    }
+    func detectLanguage() {
+        if detectLanguageCheck {
+            detectLanguageCheck = false
+        } else {
+            detectLanguageCheck = true
+        }
+    }
+}
+
+struct MenuListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuListView()
+    }
+}
