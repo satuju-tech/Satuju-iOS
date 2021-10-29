3d962b3a081811f93f067d77db74802c62eb82e1
PAW123 <gedeprama123@gmail.com>
2021-10-29 15:14:37 +0700

    half done

---
+++ Satuju/ReusableView/LanguageButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  LanguageButtonView.swift
+//  Satuju
+//
+//  Created by Gede Wicaksana on 29/10/21.
+//
+
+import SwiftUI
+
+struct LanguageButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LanguageButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageButtonView()
+    }
+}
