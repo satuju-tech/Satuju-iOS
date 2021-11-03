269aa532da30103a3e7fdd1da0e2b1e957817348
Yossan Rahmadi <yossansr06@gmail.com>
2021-11-03 07:45:42 +0700

    feat: add TranslationHistory data model

---
+++ Satuju/Vendor/Model/TranslationHistory.swift
@@ -0,0 +1,21 @@
+//
+//  TranslationHistory.swift
+//  Satuju
+//
+//  Created by Yossan Rahmadi on 01/11/21.
+//
+
+import Foundation
+import RealmSwift
+
+class TranslationHistory: Object {
+    @objc dynamic var id = UUID()
+    @objc dynamic var originLang = ""
+    @objc dynamic var destinationLang = ""
+    @objc dynamic var originText = ""
+    @objc dynamic var destinationText = ""
+
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
