c7563153d76291f6dbb448033fe263b1b885d9a6
Yossan Rahmadi <yossansr06@gmail.com>
2021-11-10 08:42:11 +0700

    feat: add action

--- Satuju/Module/Translation/View/VoiceButtonGroup.swift
+++ Satuju/Module/Translation/View/VoiceButtonGroup.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct VoiceButtonGroup: View {
+    let actionLeftVoiceButton: () -> Void
+    let actionRightVoiceButton: () -> Void
+
     var body: some View {
         HStack(spacing: 25) {
             DetectLanguageButtonView(action: {
-
+                actionLeftVoiceButton()
             })
 
             DetectLanguageButtonView(action: {
-
+                actionRightVoiceButton()
             })
         }
     }
@@ -23,6 +26,11 @@ struct VoiceButtonGroup: View {
 
 struct VoiceButtonGroup_Previews: PreviewProvider {
     static var previews: some View {
-        VoiceButtonGroup()
+        VoiceButtonGroup {
+
+        } actionRightVoiceButton: {
+
+        }
+
     }
 }
