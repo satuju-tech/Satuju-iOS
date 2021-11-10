f005c5025f89878363c57d470000eed71e5441c4
Yossan Rahmadi <yossansr06@gmail.com>
2021-11-10 09:09:33 +0700

    refactor: using rawValue instead

--- Satuju/Module/LanguageList/View/LanguageListView.swift
+++ Satuju/Module/LanguageList/View/LanguageListView.swift
@@ -60,11 +60,11 @@ struct LanguageListView: View {
                         if isOrigin {
                             leftLangCode = key
                             leftLangName = valueText
-                            leftImageName = ImageEnum(rawValue: key)?.getCountryName() ?? "all"
+                            leftImageName = ImageEnum(rawValue: key)?.rawValue ?? "all"
                         } else {
                             rightLangCode = key
                             rightLangName = valueText
-                            rightImageName = ImageEnum(rawValue: key)?.getCountryName() ?? "all"
+                            rightImageName = ImageEnum(rawValue: key)?.rawValue ?? "all"
                         }
                         showModal.toggle()
                     }, language: valueText, isSelected: isOrigin ? key.elementsEqual(SatujuApp().leftLangCode ?? ""):
