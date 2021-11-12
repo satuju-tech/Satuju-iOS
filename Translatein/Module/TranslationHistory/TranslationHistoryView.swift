f39829702aea7e54d2294318ea53a2df00027461
Yossan Rahmadi <yossansr06@gmail.com>
2021-11-12 11:36:47 +0700

    feat: add empty state of translation history

--- Translatein/Module/TranslationHistory/TranslationHistoryView.swift
+++ Translatein/Module/TranslationHistory/TranslationHistoryView.swift
@@ -14,17 +14,48 @@ struct TranslationHistoryView: View {
         translationHistoryResults: try! Realm().objects(TranslationHistory.self)
     )
 
+    @Namespace var bottomID
+
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(translationHistory.results, id: \.id) { item in
-                    TranslationBubble(
-                        isLeft: item.isLeft,
-                        textTranslationInput: item.originText,
-                        textTranslationResult: item.destinationText,
-                        destinationLangCode: item.destinationLang)
-                }
+        if translationHistory.results.isEmpty {
+            VStack {
+                Spacer()
+
+                Image("BrandIcon")
+                    .resizable()
+                    .scaledToFit()
+
+                Text("Let's start a converstation")
+                    .font(
+                        .custom("NotoSans-Regular", size: 17)
+                    )
+                    .foregroundColor(Color("DividerColor"))
+                    .multilineTextAlignment(.center)
+
+                Spacer()
             }
+            .frame(width: 129, alignment: .center)
+        } else {
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    ForEach(translationHistory.results, id: \.id) { item in
+                        TranslationBubble(
+                            isLeft: item.isLeft,
+                            textTranslationInput: item.originText,
+                            textTranslationResult: item.destinationText,
+                            destinationLangCode: item.destinationLang)
+                            .padding(.bottom, 20)
+                    }
+                }
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    maxHeight: .infinity,
+                    alignment: .top
+                )
+            }
+
         }
     }
 }
