2956248309fc2c47e6006ec1c5ef8c90b68ced9e
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-11-15 13:10:11 +0700

    fix: disable language setting when is recording

--- Translatein/Module/Translation/View/TranslationView.swift
+++ Translatein/Module/Translation/View/TranslationView.swift
@@ -21,6 +21,7 @@ struct TranslationView: View {
             Color("Off-Color").ignoresSafeArea()
             VStack(spacing: 0) {
                 LanguageSettingView()
+                    .disabled(isRecording)
                 TranslationHistoryView()
                     .padding(.top, 21)
                 ZStack(alignment: isRecording ? .center : .bottom) {
