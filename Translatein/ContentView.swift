51680af2d3826fb5b268dd7e713fbd331a920d89
Yossan Rahmadi <yossansr06@gmail.com>
2021-11-29 17:11:07 +0700

    refactor: add field date to translation history model

--- Translatein/ContentView.swift
+++ Translatein/ContentView.swift
@@ -6,8 +6,30 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ContentView: View {
+
+    init() {
+        let config = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 1,
+
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
+            migrationBlock: { _, oldSchemaVersion in
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if oldSchemaVersion < 1 {
+                    // Nothing to do!
+                    // Realm will automatically detect new properties and removed properties
+                    // And will update the schema on disk automatically
+                }
+            })
+
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = config
+    }
 
     var body: some View {
         TranslationView()
